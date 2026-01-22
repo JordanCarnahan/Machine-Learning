835f8bd65ee1f060ad2ec41e1ec2c760c43734df
Jordan Carnahan <jordanlcarnahan@gmail.com>
2026-01-22 13:37:58 -0800

    Added Files

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 # %%
 import pandas as pd
 import numpy as np
+import torch
+import torch.nn as nn
+import math
 
 df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
 arr = np.arange(10).reshape(2, 5)
 text = "hello"
 value = 42
 
+# %%
+torch.sin(torch.tensor(math.pi)) 
 
 # %%
